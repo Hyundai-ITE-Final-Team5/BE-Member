548f5fda605f76cdaffccc2de09eb373f857fdb9
hyk339 <hyk339@gmail.com>
2021-12-11 18:16:24 +0900

    refactor(homeui) : home ui 순서 조회 수정

--- ite5-be-member/src/main/java/com/mycompany/ite5bemember/controller/UiController.java
+++ ite5-be-member/src/main/java/com/mycompany/ite5bemember/controller/UiController.java
@@ -1,11 +1,12 @@
 package com.mycompany.ite5bemember.controller;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mycompany.ite5bemember.dto.HomeOrderDto;
 import com.mycompany.ite5bemember.service.UiService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +18,8 @@ public class UiController {
 	@Resource
 	private UiService uiService;
 	
-	@GetMapping("/gethomeorder")
-	public HomeOrderDto gethomeorder() {
+	@GetMapping("/gethomeorderimg")
+	public Map<String,Object> gethomeorder() {
 		return uiService.getHomeOrder();
 	}
 	
